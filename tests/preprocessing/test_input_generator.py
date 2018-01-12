45a927ebede98bda693c00a66c640f4366466ccc
vickyliin <vickyliinn@gmail.com>
2018-01-12 18:23:38 +0800

    add input generator test

---
+++ tests/preprocessing/test_input_generator.py
@@ -0,0 +1,15 @@
+from glob import glob
+import pytest
+from skimage.io import imread
+from keras_retinanet.preprocessing import InputGenerator
+
+def test_input_generator():
+    image_names = glob('tests/test-data/csv/images/*')
+    img_org = imread(image_names[0])
+    ig = InputGenerator(image_names, 
+                        batch_size=1, 
+                        image_min_side=300, 
+                        image_max_side=1024)
+    img, scale = ig.next()
+    assert min(img.shape[1:3]) == 300
+    assert scale == min(img.shape[1:3]) / min(img_org.shape[0:2])
